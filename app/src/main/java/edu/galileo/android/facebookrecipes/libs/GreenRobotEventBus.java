73d3d10f1ed55f842c82bed4a127b4831b08e998
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-23 00:05:36 -0400

    libs para EventBus igual que proyecto anterior

---
+++ app/src/main/java/edu/galileo/android/facebookrecipes/libs/GreenRobotEventBus.java
@@ -0,0 +1,23 @@
+package edu.galileo.android.facebookrecipes.libs;
+
+import edu.galileo.android.facebookrecipes.libs.base.EventBus;
+
+public class GreenRobotEventBus implements EventBus {
+    org.greenrobot.eventbus.EventBus eventBus;
+
+    public GreenRobotEventBus(org.greenrobot.eventbus.EventBus eventBus) {
+        this.eventBus = eventBus;
+    }
+
+    public void register(Object subscriber) {
+        eventBus.register(subscriber);
+    }
+
+    public void unregister(Object subscriber) {
+        eventBus.unregister(subscriber);
+    }
+
+    public void post(Object event) {
+        eventBus.post(event);
+    }
+}
---
+++ app/src/main/java/edu/galileo/android/facebookrecipes/libs/base/EventBus.java
@@ -0,0 +1,10 @@
+package edu.galileo.android.facebookrecipes.libs.base;
+
+public interface EventBus {
+    void register(Object subscriber);
+
+    void unregister(Object subscriber);
+
+    void post(Object event);
+
+}

e6ced806cb0047f6b84e752aa3ae9274a446c6f5
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-24 16:09:07 -0400

    MVP, creación de dos interacturadores

---
+++ app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/GetNextRecipeInteractor.java
@@ -0,0 +1,8 @@
+package edu.galileo.android.facebookrecipes.recipemain;
+
+/**
+ * Created by carlos.gomez on 24/06/2016.
+ */
+public interface GetNextRecipeInteractor {
+    void execute();
+}
---
+++ app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/SaveRecipeInteractor.java
@@ -0,0 +1,10 @@
+package edu.galileo.android.facebookrecipes.recipemain;
+
+import edu.galileo.android.facebookrecipes.entities.Recipe;
+
+/**
+ * Created by carlos.gomez on 24/06/2016.
+ */
+public interface SaveRecipeInteractor {
+    void execute(Recipe recipe);
+}

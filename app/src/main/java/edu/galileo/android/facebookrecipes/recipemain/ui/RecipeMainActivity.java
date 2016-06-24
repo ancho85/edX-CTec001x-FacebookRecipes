4338532b2909b7532513234d1ca427de89a5b45d
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-24 16:01:49 -0400

    nuevo paquete ui y la actividad principal se mueve allí

--- app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/ui/RecipeMainActivity.java
+++ app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/ui/RecipeMainActivity.java
@@ -1,4 +1,4 @@
-package edu.galileo.android.facebookrecipes.recipemain;
+package edu.galileo.android.facebookrecipes.recipemain.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;

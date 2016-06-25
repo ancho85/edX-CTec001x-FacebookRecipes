b72e87c1ee59e7a04ab9c40c1c4a3e5a11322454
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-25 16:20:30 -0400

    DI, método 2 de inyección de dependencias (parcial)

--- app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/di/RecipeMainComponent.java
+++ app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/di/RecipeMainComponent.java
@@ -3,7 +3,9 @@ package edu.galileo.android.facebookrecipes.recipemain.di;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import edu.galileo.android.facebookrecipes.libs.base.ImageLoader;
 import edu.galileo.android.facebookrecipes.libs.di.LibsModule;
+import edu.galileo.android.facebookrecipes.recipemain.RecipeMainPresenter;
 
 /**
  * Created by carlos.gomez on 25/06/2016.
@@ -12,4 +14,11 @@ import edu.galileo.android.facebookrecipes.libs.di.LibsModule;
 @Singleton
 @Component(modules = {RecipeMainModule.class, LibsModule.class})
 public interface RecipeMainComponent {
+    //método 1 de inyección, con inject
+    //void inject(RecipeMainActivity activity);/
+
+    //método 2 de inyección, con get<Objeto_a_injectar> y son llamados en cascada
+    //para que esto funcione hay que tener un provides que devuelve el objeto
+    ImageLoader getImageLoader();
+    RecipeMainPresenter getPresenter();
 }

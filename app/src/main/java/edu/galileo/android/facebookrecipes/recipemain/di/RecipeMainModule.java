4177c167c23a1eaf7d146d2335f2f4e718e0c74e
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-25 16:44:16 -0400

    DI, módulo actualizado para proveer el presenter, los interactors, el repository y el servicio

--- app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/di/RecipeMainModule.java
+++ app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/di/RecipeMainModule.java
@@ -4,6 +4,17 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import edu.galileo.android.facebookrecipes.api.RecipeClient;
+import edu.galileo.android.facebookrecipes.api.RecipeService;
+import edu.galileo.android.facebookrecipes.libs.base.EventBus;
+import edu.galileo.android.facebookrecipes.recipemain.GetNextRecipeInteractor;
+import edu.galileo.android.facebookrecipes.recipemain.GetNextRecipeInteractorImpl;
+import edu.galileo.android.facebookrecipes.recipemain.RecipeMainPresenter;
+import edu.galileo.android.facebookrecipes.recipemain.RecipeMainPresenterImpl;
+import edu.galileo.android.facebookrecipes.recipemain.RecipeMainRepository;
+import edu.galileo.android.facebookrecipes.recipemain.RecipeMainRepositoryImpl;
+import edu.galileo.android.facebookrecipes.recipemain.SaveRecipeInteractor;
+import edu.galileo.android.facebookrecipes.recipemain.SaveRecipeInteractorImpl;
 import edu.galileo.android.facebookrecipes.recipemain.ui.RecipeMainView;
 
 /**
@@ -19,8 +30,41 @@ public class RecipeMainModule {
 
     @Provides
     @Singleton
-    RecipeMainView providesRecipeMainView(){
+    RecipeMainView providesRecipeMainView() {
         return this.view;
+    }
+
+    @Provides
+    @Singleton
+    RecipeMainPresenter providesRecipeMainPresenter(EventBus eventBus, RecipeMainView view, SaveRecipeInteractor saveInteractor, GetNextRecipeInteractor getNextInteractor) {
+        return new RecipeMainPresenterImpl(eventBus, view, saveInteractor, getNextInteractor);
+    }
+
+    @Provides
+    @Singleton
+    SaveRecipeInteractor provideSaveRecipeInteractor(RecipeMainRepository repository) {
+        return new SaveRecipeInteractorImpl(repository);
+    }
+
+    @Provides
+    @Singleton
+    GetNextRecipeInteractor provideGetNextRecipeInteractor(RecipeMainRepository repository) {
+        return new GetNextRecipeInteractorImpl(repository);
+    }
+
+    @Provides
+    @Singleton
+    RecipeMainRepository provideRecipeMainRepository(EventBus eventBus, RecipeService service) {
+        return new RecipeMainRepositoryImpl(eventBus, service);
+    }
+
+    //EventBus ya es proveído en las librerías
+    //falta proveer el servicio que tiene que  ver con retrofit, esto podría estar también entre las librerías
+    //pero como no lo estoy usuando en otro módulo que no sea en éste, se lo coloca aquí.
+    @Provides
+    @Singleton
+    RecipeService provideRecipeService() {
+        return new RecipeClient().getRecipeService();
     }
 
 }

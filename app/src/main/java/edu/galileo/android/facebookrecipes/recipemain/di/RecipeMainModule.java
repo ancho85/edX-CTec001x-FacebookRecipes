f553e96d059ec0123474de48492b62f2ca4049b9
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-25 16:23:59 -0400

    DI, módulo actualizado para proveer RecipeMainView

--- app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/di/RecipeMainModule.java
+++ app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/di/RecipeMainModule.java
@@ -1,7 +1,26 @@
 package edu.galileo.android.facebookrecipes.recipemain.di;
 
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import edu.galileo.android.facebookrecipes.recipemain.ui.RecipeMainView;
+
 /**
  * Created by carlos.gomez on 25/06/2016.
  */
+@Module
 public class RecipeMainModule {
+    RecipeMainView view;
+
+    public RecipeMainModule(RecipeMainView view) {
+        this.view = view;
+    }
+
+    @Provides
+    @Singleton
+    RecipeMainView providesRecipeMainView(){
+        return this.view;
+    }
+
 }

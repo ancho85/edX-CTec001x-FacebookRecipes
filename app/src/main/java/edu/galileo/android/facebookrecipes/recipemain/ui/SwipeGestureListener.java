3528b613c01a33efa0f185f2037fee628e5256de
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-06-25 17:19:19 -0400

    UI, swipe detector

---
+++ app/src/main/java/edu/galileo/android/facebookrecipes/recipemain/ui/SwipeGestureListener.java
@@ -0,0 +1,10 @@
+package edu.galileo.android.facebookrecipes.recipemain.ui;
+
+/**
+ * Created by carlos.gomez on 25/06/2016.
+ */
+public interface SwipeGestureListener {
+    //interfaz para saber que hacer en el momento en que se dio el "swipe" en alguna dirección
+    void onKeep();
+    void onDismiss();
+}

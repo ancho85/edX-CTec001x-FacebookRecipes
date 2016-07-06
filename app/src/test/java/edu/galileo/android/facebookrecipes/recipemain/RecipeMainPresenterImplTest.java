79b73267b31c4deb55aaa1a9448544f438556b4b
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-07-06 11:56:18 -0400

    Prueba de presentador prueba de interactors

--- app/src/test/java/edu/galileo/android/facebookrecipes/recipemain/RecipeMainPresenterImplTest.java
+++ app/src/test/java/edu/galileo/android/facebookrecipes/recipemain/RecipeMainPresenterImplTest.java
@@ -4,9 +4,11 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import edu.galileo.android.facebookrecipes.BaseTest;
+import edu.galileo.android.facebookrecipes.entities.Recipe;
 import edu.galileo.android.facebookrecipes.libs.base.EventBus;
 import edu.galileo.android.facebookrecipes.recipemain.ui.RecipeMainView;
 
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
 import static org.mockito.Mockito.verify;
 
@@ -24,6 +26,8 @@ public class RecipeMainPresenterImplTest extends BaseTest {
     private SaveRecipeInteractor saveInteractor;
     @Mock
     private GetNextRecipeInteractor getNextInteractor;
+    @Mock
+    Recipe recipe = new Recipe();
 
     private RecipeMainPresenterImpl presenter; // este sera objecto de pruebas
 
@@ -45,5 +49,30 @@ public class RecipeMainPresenterImplTest extends BaseTest {
         verify(eventBus).unregister(presenter); // verifique que unregister fue invocado con ese parámetro
         assertNull(presenter.getView()); // se verifica que la vista también sea nula
     }
-    
+
+    @Test
+    public void testSaveRecipe_executeTestInteractor() throws Exception {
+        presenter.saveRecipe(recipe);
+        assertNotNull(presenter.getView());  // la vista no debe ser nula
+        verify(view).saveAnimation(); // se hace animación
+        verify(view).hideUIElements();  // se oculta elementos
+        verify(view).showProgress(); // se muestra el progreso
+        verify(saveInteractor).execute(recipe); // se ejecutó con parámetro recipe
+    }
+
+    @Test
+    public void testDismissRecipe_executeGetNextRecipeInteractor() throws Exception {
+        presenter.dismissRecipe();
+        assertNotNull(presenter.getView());
+        verify(view).dismissAnimation();
+    }
+
+    @Test
+    public void testGetNextRecipe_executeGextNextRecipeInteractor() throws Exception {
+        presenter.getNextRecipe();
+        assertNotNull(presenter.getView());
+        verify(view).hideUIElements();
+        verify(view).showProgress();
+        verify(getNextInteractor).execute();
+    }
 }

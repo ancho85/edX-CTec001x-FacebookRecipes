05d2388efec0f606608ef510b3a6835613c9ae1e
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-07-06 17:27:47 -0400

    prueba de repositorio para el listado, verificación de base de datos actualización y borrado

--- app/src/test/java/edu/galileo/android/facebookrecipes/recipelist/RecipeListRepositoryImplTest.java
+++ app/src/test/java/edu/galileo/android/facebookrecipes/recipelist/RecipeListRepositoryImplTest.java
@@ -18,10 +18,12 @@ import edu.galileo.android.facebookrecipes.BaseTest;
 import edu.galileo.android.facebookrecipes.BuildConfig;
 import edu.galileo.android.facebookrecipes.FacebookRecipesApp;
 import edu.galileo.android.facebookrecipes.entities.Recipe;
+import edu.galileo.android.facebookrecipes.entities.Recipe_Table;
 import edu.galileo.android.facebookrecipes.libs.base.EventBus;
 import edu.galileo.android.facebookrecipes.recipelist.events.RecipeListEvent;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -85,11 +87,49 @@ public class RecipeListRepositoryImplTest extends BaseTest {
 
     @Test
     public void testUpdateRecipe() throws Exception {
+        String newRecipeId = "id1";
+        String titleBefore = "title before update";
+        String titleAfter = "title after update";
+        Recipe recipe = new Recipe();
+        recipe.setRecipeId(newRecipeId);
+        recipe.setTitle(titleBefore);
+        recipe.save();
+        recipe.setTitle(titleAfter);
 
+        repository.updateRecipe(recipe);
+
+        Recipe recipeFromDB = new Select()
+                .from(Recipe.class)
+                .where(Recipe_Table.recipeId.is(newRecipeId)) // Recipe_Table es una clase autogenerada por DBFlow que contiene todos los atributos del POJO
+                .querySingle();
+
+        assertEquals(titleAfter, recipeFromDB.getTitle());
+        verify(eventBus).post(recipeListEventArgumentCaptor.capture());
+
+        RecipeListEvent event = recipeListEventArgumentCaptor.getValue();
+        assertEquals(RecipeListEvent.UPDATE_EVENT, event.getType());
+
+        recipe.delete();
     }
 
     @Test
     public void testRemoveRecipe() throws Exception {
+        int recipes_in_delete_event = 1;
+        String newRecipeId = "id1";
+        Recipe recipe = new Recipe();
+        recipe.setRecipeId(newRecipeId);
+        recipe.save();
 
+        repository.removeRecipe(recipe);
+
+        assertFalse(recipe.exists());
+        verify(eventBus).post(recipeListEventArgumentCaptor.capture());
+
+        RecipeListEvent event = recipeListEventArgumentCaptor.getValue();
+        assertEquals(RecipeListEvent.DELETE_EVENT, event.getType());
+        assertEquals(recipes_in_delete_event, event.getRecipeList().size());
+        assertEquals(recipe, event.getRecipeList().get(0));
+
+        recipe.delete();
     }
 }

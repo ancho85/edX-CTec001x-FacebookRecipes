9a65b02ee53b0f7e59c25305af5fec1d370f6a51
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-07-07 11:24:16 -0400

    Prueba de activity para el listado verificación de logout y main del menú

--- app/src/test/java/edu/galileo/android/facebookrecipes/recipelist/RecipeListActivityTest.java
+++ app/src/test/java/edu/galileo/android/facebookrecipes/recipelist/RecipeListActivityTest.java
@@ -1,5 +1,7 @@
 package edu.galileo.android.facebookrecipes.recipelist;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.annotation.StyleRes;
 
 import org.junit.Test;
@@ -8,6 +10,7 @@ import org.mockito.Mock;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.util.ActivityController;
 
 import java.util.List;
@@ -16,11 +19,17 @@ import edu.galileo.android.facebookrecipes.BaseTest;
 import edu.galileo.android.facebookrecipes.BuildConfig;
 import edu.galileo.android.facebookrecipes.R;
 import edu.galileo.android.facebookrecipes.entities.Recipe;
+import edu.galileo.android.facebookrecipes.libs.base.ImageLoader;
+import edu.galileo.android.facebookrecipes.login.ui.LoginActivity;
 import edu.galileo.android.facebookrecipes.recipelist.ui.RecipeListActivity;
 import edu.galileo.android.facebookrecipes.recipelist.ui.RecipeListView;
+import edu.galileo.android.facebookrecipes.recipelist.ui.adapters.OnItemClickListener;
 import edu.galileo.android.facebookrecipes.recipelist.ui.adapters.RecipesAdapter;
+import edu.galileo.android.facebookrecipes.recipemain.ui.RecipeMainActivity;
 
+import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.robolectric.Shadows.shadowOf;
 
 /**
  * Created by carlos.gomez on 07/07/2016.
@@ -33,17 +42,22 @@ public class RecipeListActivityTest extends BaseTest {
     @Mock
     private List<Recipe> recipeList;
     @Mock
+    private ImageLoader imageLoader;
+    @Mock
     private RecipesAdapter adapter;
     @Mock
     private RecipeListPresenter presenter;
 
     private RecipeListView view;
+    private RecipeListActivity activity;
+    private OnItemClickListener onItemClickListener;
+    private ShadowActivity shadowActivity;
     private ActivityController<RecipeListActivity> controller;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        RecipeListActivity recipeListActivity = new RecipeListActivity(){
+        RecipeListActivity recipeListActivity = new RecipeListActivity() {
 
             @Override
             public void setTheme(@StyleRes int resid) {
@@ -62,7 +76,11 @@ public class RecipeListActivityTest extends BaseTest {
         };
 
         controller = ActivityController.of(Robolectric.getShadowsAdapter(), recipeListActivity).create().visible();
-        view = (RecipeListView) controller.get();
+        activity = controller.get(); //sin type casting
+        view = (RecipeListView) controller.get(); // type casting redundante, pero hay que definirlo
+        onItemClickListener = (OnItemClickListener) activity; // type casting redundante, pero hay que definirlo
+
+        shadowActivity = shadowOf(activity);
     }
 
     //se ignoran pruebas para verificar que el presentador llame al onCreate y onDestroy... ver otro test
@@ -83,5 +101,21 @@ public class RecipeListActivityTest extends BaseTest {
     public void testRecipeDeleted_shouldUpdateAdapter() throws Exception {
         view.recipeDeleted(recipe);
         verify(adapter).removeRecipe(recipe);
+    }
+
+    @Test
+    public void testLogoutMenuClicked_ShouldLaunchLoginActivity() throws Exception {
+        shadowActivity.clickMenuItem(R.id.action_logout);  // simulación de click de menú logout
+        Intent intentAfterLogout = shadowActivity.peekNextStartedActivity();  //obtener la actividad siguiente
+        ComponentName loginComponent = new ComponentName(activity, LoginActivity.class);  //contexto y clase al ComponentName
+        assertEquals(loginComponent, intentAfterLogout.getComponent());
+    }
+
+    @Test
+    public void testMainMenuClicked_ShouldLaunchRecipeMainActivity() throws Exception {
+        shadowActivity.clickMenuItem(R.id.action_main);  // simulación de click de menú lista
+        Intent intentAfterLogout = shadowActivity.peekNextStartedActivity();  //obtener la actividad siguiente
+        ComponentName loginComponent = new ComponentName(activity, RecipeMainActivity.class);  //contexto y clase al ComponentName
+        assertEquals(loginComponent, intentAfterLogout.getComponent());
     }
 }
